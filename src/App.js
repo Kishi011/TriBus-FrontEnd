83d362c7db84ce28a9d2a66d1b5af2062c1cef78
Vinicius Mulinari Stuani <viniciusmulinari.st@gmail.com>
2022-10-26 11:47:14 -0300

    criação do projeto e página inicial

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="principal">
+
+        </div>
+        <div className="login-google">
+
+        </div>
+        <div className="login-facebook">
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;

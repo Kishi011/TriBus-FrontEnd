acb83c8206d7dac3df7fca5b9a04c917aa7e8da8
Vinicius Mulinari Stuani <viniciusmulinari.st@gmail.com>
2022-10-30 00:22:03 -0300

    login com google, ainda falta fazer funcionar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
 import './App.css';
+import { useState } from 'react';
+import GoogleLogin from 'react-google-login';
 
 function App() {
+
+  const [loginData, setLoginData] = useState(
+    localStorage.getItem('loginData')
+    ? JSON.parse(localStorage.getItem('loginData'))
+    : null
+  );
+
+  const handleFailure = (result) => {
+    alert(result);
+  };
+
+  const handleLogin = (googleData) => {
+    console.log(googleData);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem('loginData');
+    setLoginData(null);
+  };
+
   return (
     <div className="App">
       <div className="container">
         <div className="principal">
-
+          <h1>Titulo</h1>
+          <img alt='logo tribus'/>
         </div>
         <div className="login-google">
-
+          {
+            loginData ? (
+              <div>
+                <h3>VOCÊ LOGOU COMO {loginData.email}</h3>
+                <button onClick={handleLogout}>Deslogar</button>
+              </div>
+            ) : (
+              <GoogleLogin
+                clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+                buttonText='Entrar com o Google'
+                onSuccess={handleLogin}
+                onFailure={handleFailure}
+                cookiePolicy={'single_host_origin'}
+              />
+            )
+          }          
         </div>
         <div className="login-facebook">
 
